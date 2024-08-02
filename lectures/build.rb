657cb7da82ce35284f91e64dcc03c143988614d5
pintergreg <pintergreg@gmail.com>
2024-08-02 15:03:26 +0200

    Use config file for the build script #3

--- lectures/build.rb
+++ lectures/build.rb
@@ -1,16 +1,10 @@
 require 'optparse'
+require 'yaml'
 
-options = {
-  self_contained: false,
-  markdown: 'mpd_applications'.freeze,
-  bibliography: 'references.bib'.freeze,
-  # cls: 'assets/apa.csl'.freeze,
-  cls: '../assets/ieee.csl'.freeze,
-  reveal_url: "".freeze,
-  assets: "../assets".freeze
-}
+options = YAML.load_file('build_config.yaml')
+puts options
 
-options[:reveal_url] = "http://127.0.0.1/reveal.js-5.1.0/"
+#options[:reveal_url] = "http://127.0.0.1/reveal.js-5.1.0/"
 # options[:self_contained] = true
 
 OptionParser.new do |parser|
@@ -25,8 +19,8 @@ OptionParser.new do |parser|
   parser.on('--bibliography BIBLIOGRAPHY', 'Provide BIBLIOGRAPHY file') do |bibliography|
     options[:bibliography] = bibliography
   end
-  parser.on('--cls CLS', 'Provide CLS file') do |cls|
-    options[:cls] = cls
+  parser.on('--csl CSL', 'Provide CSL file') do |csl|
+    options[:csl] = csl
   end
   parser.on('-s', '--self-contained', 'Pass self-contained argument to pandoc') do |_s|
     options[:self_contained] = true
@@ -36,6 +30,7 @@ OptionParser.new do |parser|
   end
 end.parse!
 
+p options
 if options[:markdown] == "README"
   exit(0)
 end
@@ -43,7 +38,7 @@ end
 if options[:self_contained]
   `pandoc -f markdown+tex_math_double_backslash -t revealjs #{options[:markdown]}.md --standalone --slide-level 2 --css #{options[:assets]}/custom.css -o #{options[:markdown]}_embedded.html --citeproc --csl #{options[:cls]} --bibliography #{options[:bibliography]} --bibliography wikipedia.bib --mathml --embed-resources #{options[:reveal_url]}`
 else
-  `pandoc -f markdown+tex_math_double_backslash -t revealjs #{options[:markdown]}.md --standalone --slide-level 2 --css #{options[:assets]}/custom.css -o #{options[:markdown]}.html --citeproc --csl #{options[:cls]} --bibliography #{options[:bibliography]} --bibliography wikipedia.bib --mathml #{options[:reveal_url]} -H #{options[:assets]}/custom_header.html -A #{options[:assets]}/custom_after_body.html -L #{options[:assets]}/highlight_code.lua`
+  `pandoc -f markdown+tex_math_double_backslash -t revealjs #{options[:markdown]}.md --standalone --slide-level 2 --css #{options[:assets]}/custom.css -o #{options[:markdown]}.html --citeproc --csl #{options["csl"]} --bibliography #{options["bibliography"]} --bibliography wikipedia.bib --mathml #{options[:reveal_url]} -H #{options["assets"]}/custom_header.html -A #{options["assets"]}/custom_after_body.html `#-L #{options[:assets]}/highlight_code.lua`
   # `pandoc -f markdown+tex_math_double_backslash -t revealjs #{options[:markdown]}.md --standalone --slide-level 2 --css #{options[:assets]}/custom.css -o #{options[:markdown]}_print.html --citeproc --csl #{options[:cls]} --bibliography #{options[:bibliography]} --bibliography wikipedia.bib --bibliography wikipedia.bib --mathml #{options[:reveal_url]}`
 end
  # -L ../assets/revealjs-codeblock.lua
