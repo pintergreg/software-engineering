b061a65014ac1639e495c9dc7bd7557fd903b45e
pintergreg <pintergreg@gmail.com>
2024-08-07 11:36:44 +0200

    Create output dir #5

--- build.rb
+++ build.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
+require 'fileutils'
+
+@target = 'src'
+
 chapters = []
 File.readlines('lectures/SUMMARY.md').each do |line|
   next unless m = line.strip.match(%r{\[.*\]\((lectures/.+\.md)\)})
 
   chapters.push m.captures[0]
 end
+
+FileUtils.rm_r @target if Dir.exist?(@target)
+FileUtils.mkdir_p @target
