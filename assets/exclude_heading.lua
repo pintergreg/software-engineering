77ee9c7002d12dac1a739c3ffd47b4e356d2f93b
pintergreg <pintergreg@gmail.com>
2024-08-07 14:50:36 +0200

    Write filter to exclude header and div with a given class #5

--- assets/exclude_heading.lua
+++ assets/exclude_heading.lua
@@ -1,9 +1,18 @@
 if FORMAT:match 'commonmark' then
     function Header(block)
-        for _, class in ipairs(block.classes) do
-            if class == 'hide-header' then
-                return ""
-            end
+        if block.attr.classes:includes('hide-header') then
+            return ""
+        end
+        if block.attr.classes:includes('hide-slide') then
+            --             print(block.content)
+            --             block.content = pandoc.RawInline('markdown', '')
+            return ""
+        end
+    end
+
+    function Div(div)
+        if div.attr.classes:includes('exclude') then
+            return ""
         end
     end
 end
