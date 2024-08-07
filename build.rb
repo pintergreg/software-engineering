b2d36ec676446a27c8c2ef110362ef9bcd88944e
pintergreg <pintergreg@gmail.com>
2024-08-07 11:33:30 +0200

    Remove experiment lines #5

--- build.rb
+++ build.rb
@@ -2,7 +2,4 @@ chapters = []
 File.readlines('lectures/SUMMARY.md').each do |line|
     next unless m = line.strip.match(/\[.*\]\((lectures\/.+\.md)\)/)
     chapters.push m.captures[0]
-    # if line =~ /\[.*\]\((lectures\/.+\.md)\)/
-    #     p line
-    # end
 end
