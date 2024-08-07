553ddcea4abc7273cdff1b320871749e0f72a2ed
pintergreg <pintergreg@gmail.com>
2024-08-07 11:34:15 +0200

    Apply rubocop #5

--- build.rb
+++ build.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 chapters = []
 File.readlines('lectures/SUMMARY.md').each do |line|
-    next unless m = line.strip.match(/\[.*\]\((lectures\/.+\.md)\)/)
-    chapters.push m.captures[0]
+  next unless m = line.strip.match(%r{\[.*\]\((lectures/.+\.md)\)})
+
+  chapters.push m.captures[0]
 end
