929387300b8ddc121b5d21df2f428b65e9eb6d99
pintergreg <pintergreg@gmail.com>
2024-08-07 11:33:11 +0200

    Collect chapters from SUMMARY.md #5

---
+++ build.rb
@@ -0,0 +1,8 @@
+chapters = []
+File.readlines('lectures/SUMMARY.md').each do |line|
+    next unless m = line.strip.match(/\[.*\]\((lectures\/.+\.md)\)/)
+    chapters.push m.captures[0]
+    # if line =~ /\[.*\]\((lectures\/.+\.md)\)/
+    #     p line
+    # end
+end
