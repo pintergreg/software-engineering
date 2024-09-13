beb096572b326f873a5e24f2a09ab5c993cfa378
pintergreg <pintergreg@gmail.com>
2024-09-13 18:08:51 +0200

    Update build script #5
    - use string keys
    - update to --embed-resources
    - remove reveal-url

--- lectures/build.rb
+++ lectures/build.rb
@@ -3,39 +3,40 @@ require 'yaml'
 
 options = YAML.load_file('build_config.yaml')
 
-#options[:reveal_url] = "http://127.0.0.1/reveal.js-5.1.0/"
-
 OptionParser.new do |parser|
   parser.banner = 'Usage: build.rb [options]'
 
   parser.on('-m', '--markdown INPUT', 'Provide MARKDOWN filename') do |i|
-    options[:markdown] = i
+    options["markdown"] = i
     if i.end_with? '.md'
-      options[:markdown] = i[..-4]
+      options["markdown"] = i[..-4]
     end
   end
   parser.on('--bibliography BIBLIOGRAPHY', 'Provide BIBLIOGRAPHY file') do |bibliography|
-    options[:bibliography] = bibliography
+    options["bibliography"] = bibliography
   end
   parser.on('--csl CSL', 'Provide CSL file') do |csl|
-    options[:csl] = csl
+    options["csl"] = csl
   end
-  parser.on('-s', '--self-contained', 'Pass self-contained argument to pandoc') do |_s|
-    options[:self_contained] = true
+  parser.on('-e', '--embed-resources', 'embed resources') do |_s|
+    options["embed_resources"] = true
   end
-  parser.on('--reveal-url REVEALURL', 'RevealJS URL') do |url|
-    options[:reveal_url] = "-V revealjs-url:\"#{url}\""
+  parser.on('--revealjs-url REVEALURL', 'RevealJS URL') do |url|
+    options["revealjs_url"] = "-V revealjs-url:\"#{url}\""
   end
 end.parse!
 
 options["exclude"].each do |filename|
-  if options[:markdown] == filename
+  if options["markdown"] == filename
     exit(0)
   end
 end
 
 arguments = "-f markdown+tex_math_double_backslash -t revealjs"
-arguments += " #{options[:markdown]}.md -o #{options[:markdown]}.html"
+arguments += " #{options["markdown"]}.md -o #{options["markdown"]}.html"
+if options["embed_resources"]
+  arguments += " --embed-resources"
+end
 arguments += " --standalone"
 arguments += " --slide-level 2"
 arguments += " --css #{options["assets"]}/custom.css"
@@ -46,8 +47,8 @@ options["bibliography"].each do |bibfile|
   arguments += " --bibliography #{bibfile}"
 end
 if options["mathml"]
-  arguments += " --mathml #{options[:reveal_url]}"
+  arguments += " --mathml"
 end
-arguments += " -H #{options["assets"]}/custom_header.html -A #{options["assets"]}/custom_after_body.html"
+# arguments += " -H #{options["assets"]}/custom_header.html -A #{options["assets"]}/custom_after_body.html"
 
 `pandoc #{arguments}`
