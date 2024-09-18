70d562dcd435a2de0b69e9bf95d3e7d419c2325b
pintergreg <pintergreg@gmail.com>
2024-09-18 19:10:15 +0200

    Find all summary slides #5

--- lectures/build_slides.rb
+++ lectures/build_slides.rb
@@ -3,9 +3,10 @@ inputs = [
     "01_sdlc.md",
     "02_scrum.md",
     "03_kanban.md",
-    "summary_week01.md",
-    "summary_week02.md",
 ]
+Dir.glob('./summary_week*.md') do |x|
+    inputs.push x
+end
 inputs.each do |x|
     `ruby build.rb -m #{x}`
 end
