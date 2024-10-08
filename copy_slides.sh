720d3d1751613224517a06d6f971c0bc54491659
pintergreg <pintergreg@gmail.com>
2024-10-08 18:25:37 +0200

    Remove copy_slides.sh #5

--- copy_slides.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-mkdir -p book/slides/figures
-mkdir -p book/slides/assets
-cp -r lectures/figures/* book/slides/figures/
-cp -r lectures/*.html book/slides/
-cp -r assets/* book/assets/
