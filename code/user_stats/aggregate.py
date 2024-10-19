d406ddfce4cfff38790cc07689c048c9ac3b467c
pintergreg <pintergreg@gmail.com>
2024-10-19 16:01:27 +0200

    Generate empty dataframe #27

--- code/user_stats/aggregate.py
+++ code/user_stats/aggregate.py
@@ -4,5 +4,29 @@ import pandas as pd
 con = sqlite3.connect("data.db")
 
 query = f"SELECT * FROM activity WHERE user_id = {42}"
+
+
+query = """
+SELECT
+    CAST(strftime('%W', timestamp) AS INTEGER) AS week_of_year,
+    CAST(strftime('%u', timestamp) AS INTEGER) AS day_of_week,
+    count(*) AS count
+FROM activity
+WHERE
+    user_id = 42 AND
+    week_of_year > 35 AND
+    week_of_year < 40
+GROUP BY
+    week_of_year,
+    day_of_week
+;
+"""
 data = pd.read_sql(query, con)
-print(data)
+
+records = []
+for woy in range(37, 40):
+    for dow in range(1, 8):
+        records.append([woy, dow, 0])
+empty = pd.DataFrame.from_records(
+    records, columns=["week_of_year", "day_of_week", "count"]
+)
