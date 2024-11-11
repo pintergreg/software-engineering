563f40eb5cfc26702c887a9f9a68b92f67a43971
pintergreg <pintergreg@gmail.com>
2024-11-11 09:55:52 +0100

    Copy presentation content from all collected summaries #24

--- lectures/build_summary.rb
+++ lectures/build_summary.rb
@@ -8,22 +8,21 @@ def collect_summaries()
     return result.sort
 end
 
-
 FileUtils.remove_file "summary_full.md", force=true
-preamble = false
-refs = false
-File.readlines('summary_week01.md').each_with_index do |line, i|
-    if line.start_with? "# references" then
-        refs = true
-    end
-    File.open("summary_full.md", "a") do |fp|
-        if not preamble and i > 0 and not refs
-            fp.write line
+
+summaries = collect_summaries
+summaries.each do |summary|
+    preamble = false
+    refs = false
+    File.readlines(summary).each_with_index do |line, i|
+        refs = true if line.start_with? "# references"
+        
+        File.open("summary_full.md", "a") do |fp|
+            if not preamble and i > 0 and not refs
+                fp.write line
+            end
         end
+        
+        preamble = !preamble if line.strip == "---"
     end
-    if line.strip == "---"
-        preamble = !preamble
-        # p line, i, preamble
-    end
- 
 end
