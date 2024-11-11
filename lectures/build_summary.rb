de109361c456699b3b43a5d45645b0e7e36d467e
pintergreg <pintergreg@gmail.com>
2024-11-11 09:46:25 +0100

    Copy presentation content before references slide #24

--- lectures/build_summary.rb
+++ lectures/build_summary.rb
@@ -8,9 +8,13 @@ inputs.sort!
 
 FileUtils.remove_file "summary_full.md", force=true
 preamble = false
+refs = false
 File.readlines('summary_week01.md').each_with_index do |line, i|
+    if line.start_with? "# references" then
+        refs = true
+    end
     File.open("summary_full.md", "a") do |fp|
-        if not preamble and i > 0
+        if not preamble and i > 0 and not refs
             fp.write line
         end
     end
