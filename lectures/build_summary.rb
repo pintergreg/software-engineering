8ae6d77c57f9db2b6c9fbae3a30be9cf8aaf2c80
pintergreg <pintergreg@gmail.com>
2024-11-12 14:31:42 +0100

    Split script into functions #24

--- lectures/build_summary.rb
+++ lectures/build_summary.rb
@@ -8,33 +8,44 @@ def collect_summaries()
     return result.sort
 end
 
-FileUtils.remove_file "summary_full.md", force=true
-
-File.open("summary_full.md", "a") do |fp|
-    File.readlines("summary_header.md").each do |line|
-        fp.write line
-    end
-end
-
-summaries = collect_summaries
-summaries.each do |summary|
-    preamble = false
-    refs = false
-    File.readlines(summary).each_with_index do |line, i|
-        refs = true if line.start_with? "# references"
-        
-        File.open("summary_full.md", "a") do |fp|
-            if not preamble and i > 0 and not refs
-                fp.write line
-            end
+def write_header()
+    File.open("summary_full.md", "a") do |fp|
+        File.readlines("summary_header.md").each do |line|
+            fp.write line
         end
-        
-        preamble = !preamble if line.strip == "---"
     end
 end
 
-File.open("summary_full.md", "a") do |fp|
-    fp.write "# references\n\n"
-    fp.write "::: {#refs .text-smaller}\n"
-    fp.write ":::\n"
+def write_content()
+    summaries = collect_summaries
+    summaries.each do |summary|
+        preamble = false
+        refs = false
+        File.readlines(summary).each_with_index do |line, i|
+            refs = true if line.start_with? "# references"
+            
+            File.open("summary_full.md", "a") do |fp|
+                if not preamble and i > 0 and not refs
+                    fp.write line
+                end
+            end
+            
+            preamble = !preamble if line.strip == "---"
+        end
+    end
+end
+
+def write_references()
+    File.open("summary_full.md", "a") do |fp|
+        fp.write "# references\n\n"
+        fp.write "::: {#refs .text-smaller}\n"
+        fp.write ":::\n"
+    end
+end
+
+if $PROGRAM_NAME == __FILE__
+    FileUtils.remove_file "summary_full.md", force=true
+    write_header
+    write_content
+    write_references
 end
