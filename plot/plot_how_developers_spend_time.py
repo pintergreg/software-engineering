446a9a0b8414c9e5a7e88f5d5caacc88e5141ad5
pintergreg <pintergreg@gmail.com>
2025-08-22 12:42:21 +0200

    Remove separate rectangle calculation GH-30

--- plot/plot_how_developers_spend_time.py
+++ plot/plot_how_developers_spend_time.py
@@ -21,32 +21,6 @@ labels = [
 colors = ["#dae8fc", "#ffe6cc", "#f8cecc", "#d5e8d4", "#e1d5e7", "#fff2cc"]
 sizes = [32, 23, 19, 12, 9, 4]
 
-# these values define the coordinate system for the returned rectangles
-# the values will range from x to x + width and y to y + height
-x = 0.0
-y = 0.0
-width = 100
-height = 100
-
-# values = sizes
-#
-# # values must be sorted descending (and positive, obviously)
-# values.sort(reverse=True)
-#
-# # the sum of the values must equal the total area to be laid out
-# # i.e., sum(values) == width * height
-# values = squarify.normalize_sizes(values, width, height)
-#
-# # returns a list of rectangles
-rects = squarify.squarify(sizes, x, y, width, height)
-print(rects)
-#
-# # padded rectangles will probably visualize better for certain cases
-# padded_rects = squarify.padded_squarify(values, x, y, width, height)
-#
-#
-# with open("out.json", "w") as fp:
-#     json.dump(rects, fp, indent=1)
 
 fig, ax = plt.subplots(figsize=(6, 5), layout="constrained", dpi=150)
 fig.set_facecolor("none")
