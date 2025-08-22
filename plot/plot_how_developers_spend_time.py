012180c235e8f04bf25d1c42cb044f651c2918a2
pintergreg <pintergreg@gmail.com>
2025-08-22 13:08:49 +0200

    Finish adjustments GH-30

--- plot/plot_how_developers_spend_time.py
+++ plot/plot_how_developers_spend_time.py
@@ -1,8 +1,9 @@
 import squarify
 import json
-import matplotlib.pyplot as plt
-import yaml
 import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib.patches import Patch
+import yaml
 
 mpl.rcParams["svg.hashsalt"] = "42"
 
@@ -11,7 +12,7 @@ with open("plotting_config.yaml", "r") as fp:
 
 
 labels = [
-    "writing new code or improving existing code",
+    "writing new code\nor improving existing code",
     "meetings and management",
     "code maintenance",
     "testing",
@@ -19,12 +20,18 @@ labels = [
     "security issues",
 ]
 colors = ["#dae8fc", "#ffe6cc", "#f8cecc", "#d5e8d4", "#e1d5e7", "#fff2cc"]
-sizes = [32, 23, 19, 12, 9, 4]
+edge_colors = ["#dae8fc", "#ffe6cc", "#f8cecc", "#d5e8d4", "#e1d5e7", "#fff2cc"]
+sizes = [32, 23, 19, 12, 10, 4]
 
+legend_elements = []
+for label, color, edge in zip(labels, colors, edge_colors):
+    elem = Patch(fc=color, ec=edge, label=label)
+    legend_elements.append(elem)
 
 fig, ax = plt.subplots(figsize=(6, 5), layout="constrained", dpi=150)
 fig.set_facecolor("none")
-ax.set_aspect(1 / 1.2)
+# ax.set_aspect(1 / 1.2)
+ax.set_aspect("equal")
 squarify.plot(
     sizes,
     norm_x=100,
@@ -34,9 +41,23 @@ squarify.plot(
     ax=ax,
 )
 ax.invert_yaxis()
+legend = ax.legend(
+    handles=legend_elements,
+    ncols=1,
+    fontsize=8,
+    loc="upper right",
+    bbox_to_anchor=(1.575, 1),
+)
+legend.get_frame().set_alpha(None)
 
-for pos, s in zip([(5, 5), (5, 64), (60, 5), (60, 48), (60, 75), (90, 75)], sizes):
-    ax.text(pos[0], pos[1], s, ha="left", va="top", fontsize=32, fontweight="bold")
-# ax.axis("off")
+# positions = [(5, 5), (5, 64), (60, 5), (60, 48), (60, 75), (90, 75)]
+positions = [(3, 3), (3, 62), (58, 3), (58, 46), (58, 73), (89, 73)]
+for pos, s in zip(positions, sizes):
+    ax.text(pos[0], pos[1], f"{s}", ha="left", va="top", fontsize=28)
+ax.axis("off")
 for i in config["formats"]:
-    fig.savefig(f"how_developers_spend_time.{i}", metadata=config["metadata"][i])
+    fig.savefig(
+        f"../lectures/figures/how_developers_spend_time.{i}",
+        metadata=config["metadata"][i],
+        bbox_inches="tight",
+    )
