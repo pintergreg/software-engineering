a2ade22689ba1b19830f8db502e2cb67c6146d32
pintergreg <pintergreg@users.noreply.github.com>
2025-09-30 10:30:30 +0000

    deploy: 59d5036a01674cd3785b5cdd6ba83e67dd52e5e0

---
+++ lectures/figures/gantt_semester.mm
@@ -0,0 +1,8 @@
+gantt
+    title gantt diagram example
+    dateFormat YYYY-MM-DD
+    section semester
+        requirements analysis :ra, 2025-09-17, 5w
+        requirements analysis presentation: milestone,
+        design :after ra, 7w
+        design presentation: milestone,
