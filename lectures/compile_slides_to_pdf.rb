f1ad1e37e8ba77db7a7365349a4813219b2e3baa
pintergreg <pintergreg@gmail.com>
2026-08-03 17:23:26 +0200

    Add CLI to select a single target or compile all of them GH-31

--- lectures/compile_slides_to_pdf.rb
+++ lectures/compile_slides_to_pdf.rb
@@ -1,6 +1,21 @@
-inputs = Dir.glob('*.html').filter{|x| x =~ /^[0-9]{2}_.+\.html/}
+require 'optparse'
 
-inputs.each do |x|
-    # puts "#{x} #{x.gsub(".html", ".pdf")}"
-    `npx decktape reveal --headless new --chrome-path /bin/chromium #{x} #{x.gsub(".html", ".pdf")}`
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: example.rb [options]"
+
+  opts.on("-s", "--selected PATH", "Select slides to compile") do |x|
+    options[:selected] = x
+  end
+end.parse!
+
+if options.include? :selected
+    `npx decktape reveal --headless new --chrome-path /bin/chromium #{options[:selected]} #{options[:selected].gsub(".html", ".pdf")}`
+else
+    inputs = Dir.glob('*.html').filter{|x| x =~ /^[0-9]{2}_.+\.html/}
+
+    inputs.each do |x|
+        # puts "#{x} #{x.gsub(".html", ".pdf")}"
+        `npx decktape reveal --headless new --chrome-path /bin/chromium #{x} #{x.gsub(".html", ".pdf")}`
+    end
 end
